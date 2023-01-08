7db5d92e6097f80097bd31c2a7245220300cb39e
OneCreativeStudio <dev@onecreativestudio.com>
2023-01-07 19:28:27 -0500

    edited sample data array

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
+# 2022 ReactJS Frontend
 
-# Docker Execution Intructions
+## System Requirements
 
-Recommended: `docker-compose up -d --build`
+- Unix Terminal (Bash/sh/Zsh)
+- Docker & Docker Compose
+- Git (for Development)
+
+
+## Docker Execution Intructions
+
+### Recommended: `docker-compose up -d --build`
 
 Alternative: 
 
@@ -14,3 +22,6 @@ i.e.: `docker build -t frontend:v0.1 . -f ./Dockerfile`
 i.e.: `docker run -it --rm -p 3000:3000 frontend:v0.1`
 
 Execute command within container: `docker exec -t -i mycontainer /bin/sh`
+
+
+Note: See (pruning)[https://docs.docker.com/config/pruning/] for details on how to clean local environment.
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,26 +4,47 @@ function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>;
 }
 
+function Footer({ footer_text: text }) {
+  return <h4>{text ? text : 'Default footer'}</h4>;
+}
+
 export default function HomePage() {
-  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+  const scientists = [
+    {
+      name: 'Ada Lovelace',
+      role: 'creating the first computer program.'
+    },
+    {
+      name: 'Grace Hopper',
+      role: 'inventing computing methods and devices, like the compiler.'
+    },
+    {
+      name: 'Margaret Hamilton',
+      role: 'developing programs that influence how today\'s computers work.'
+    },
+  ];
+
   const [likes, setLikes] = useState(0);
 
   function handleClick() {
     setLikes(likes + 1);
     console.log("TEST SENDING CONSOLE LOG\n")
+    scientists.forEach((scientist) => console.log(`${scientist.name}: ${scientist.role}\n`));
   }
 
   return (
-    <div>
-      <Header title="🚀 Code To The 🌝" />
-      <Header title="Ship Code To The Moon!" />
-      <ul>
-        {names.map((name) => (
-          <li key={name}>{name}</li>
-        ))}
-      </ul>
-
-      <button onClick={handleClick}> Like! ({likes})</button>
-    </div>
+    <>
+      <div>
+        <Header title="🚀 Code To The 🌝 !" />
+        <ul>
+          {scientists.map((scientist) => (
+            <li key={scientist.name}>{scientist.name} was known for {scientist.role}</li>
+          ))}
+        </ul>
+        <button onClick={handleClick}> Like! ({likes})</button>
+        <Footer footer_text="Shipped Code To The Moon!" />
+      </div>
+    </>
   );
 }
