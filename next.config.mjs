34e2d92cea7417c7cff81fd15f912939464d08c7
OneCreativeStudio <dev@onecreativestudio.com>
2023-02-10 22:40:09 -0500

    removed .next dir after lint

---
+++ next.config.mjs
@@ -0,0 +1,18 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+  // webpackDevMiddleware: (config) => {
+  //   config.watchOptions = {
+  //     poll: 5000,
+  //     aggregateTimeout: 100,
+  //   };
+  //   return config;
+  // },
+  // future: {
+  //   webpack5: true,
+  // },
+};
+
+export default nextConfig;
